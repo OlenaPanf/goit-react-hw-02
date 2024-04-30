67a4e24acd42e2a8f9d7124e2b743dab57e4a2ea
OlenaPanf <elena.el@ukr.net>
2024-04-30 22:51:59 +0300

    Description.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 //import { useState } from 'react'
 import './App.css'
+import Description from '../Description/Description'
 
 export default function App() {
   
   return (
     <>
-      
+     <Description /> 
         
     </>
   )
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,13 @@
+export default function Description() {
+    const titleText = "Sip Happens Café";
+    const paragraphText = "Please leave your feedback about our service by selecting one of the options below.";
+    
+    return (
+        <>
+            <div>
+                <h1>{titleText}</h1>
+                <p>{paragraphText}</p>
+            </div>
+        </>
+    );
+}
